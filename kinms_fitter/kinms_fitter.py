Output bug fix and added units

kinms_fitter/kinms_fitter.py
@@ -296,8 +296,15 @@ class kinms_fitter:
         labels=np.array(["PA","Xc","Yc","Vsys","inc","totflux","veldisp"])
         fixed= minimums == maximums
         priors=np.array([self.pa_prior,self.xc_prior,self.yc_prior,self.vsys_prior,self.inc_prior,self.totflux_prior,self.velDisp_prior])#np.resize(None,fixed.size)
-        precision=(maximums-minimums)/10.        
-
+        precision=(maximums-minimums)/10. 
+               
+        
+        if 'beam' in self.bunit:
+            newunit=(self.spectralcube.unit*u.beam*u.km/u.s).to_string()
+        else:
+            newunit=(self.spectralcube.unit*u.km/u.s).to_string()
+            
+        units=np.array(['Deg','Deg','Deg','km/s','Deg',newunit,'km/s'])
         
         vars2look=[]
         if len(self.skySampClouds) == 0:
@@ -316,10 +323,12 @@ class kinms_fitter:
             priors=np.append(priors,np.concatenate([i.priors for i in list_vars]))
             precision=np.append(precision,np.concatenate([i.precisions for i in list_vars]))
             labels=np.append(labels,np.concatenate([i.labels for i in list_vars]))
+            units=np.append(units,np.concatenate([i.units for i in list_vars]))
             
         if np.any(self.initial_guesses != None):
             initial_guesses= self.initial_guesses
-        return initial_guesses,labels,minimums,maximums,fixed, priors,precision
+ 
+        return initial_guesses,labels,minimums,maximums,fixed, priors,precision,units
         
                      
     def clip_cube(self):
@@ -515,7 +524,7 @@ class kinms_fitter:
         pl.makeplots(block=block,plot2screen=self.show_plots)
         self.mask_sum=pl.mask.sum()
     
-    def write_text(self,bestvals, errup,errdown, fixed,runtime,errors_warnings='None',fname="KinMS_fitter_output.txt"):
+    def write_text(self,bestvals, errup,errdown,units, fixed,runtime,errors_warnings='None',fname="KinMS_fitter_output.txt"):
         if isinstance(errup,int):
             # simple mode used
             errup=['--']*bestvals.size
@@ -525,7 +534,7 @@ class kinms_fitter:
             mode='MCMC'
         
 
-        t=Table([self.labels,bestvals,errup,errdown,fixed],names=('Quantity', 'Best-fit', 'Error-up', 'Error-down','Fixed'))
+        t=Table([self.labels,bestvals,errup,errdown,units,fixed],names=('Quantity', 'Best-fit', 'Error-up', 'Error-down','Units','Fixed'))
         # for col in t.itercols():
         #    if col.info.dtype.kind == 'f':
         #        col.info.format = '.2f'
@@ -554,7 +563,7 @@ class kinms_fitter:
             t.meta['comments'].append('                 '+errmsg)
         t.meta['comments'].append('')
                 
-        t.write(fname,format='ascii.fixed_width_two_line',comment='#')
+        t.write(fname,format='ascii.fixed_width_two_line',comment='#',overwrite=True)
         
             
     def run(self,method='mcmc',justplot=False,**kwargs):
@@ -589,7 +598,7 @@ class kinms_fitter:
             self.n_radmotionvars = np.sum([i.freeparams for i in self.radial_motion])
         
         
-        initial_guesses,labels,minimums,maximums,fixed, priors,precision = self.setup_params()
+        initial_guesses,labels,minimums,maximums,fixed, priors,precision,units = self.setup_params()
         self.labels=labels
         
         
@@ -654,7 +663,13 @@ class kinms_fitter:
             
             bestvals[1]=(bestvals[1]/3600.)+self.xc_img
             bestvals[2]=(bestvals[2]/3600.)+self.yc_img
+            if (method=='mcmc'):
+                besterrs[1]=besterrs[1]/3600.
+                besterrs[2]=besterrs[2]/3600.
+            
             if (method=='mcmc')&(self.save_all_accepted):
+                besterrs[1]=besterrs[1]/3600.
+                besterrs[2]=besterrs[2]/3600.
                 np.savez(self.pdf_rootname+".npz",bestvals=bestvals, besterrs=besterrs, outputvalue=outputvalue, outputll=outputll,fixed=fixed,labels=self.labels)
 
                                         
@@ -672,7 +687,7 @@ class kinms_fitter:
                 else:
                     sig_bestfit_up=sig_bestfit_down=0
                     
-                self.write_text(bestvals, sig_bestfit_up, sig_bestfit_down, fixed,runtime,errors_warnings=self.errors_warnings,fname=fname)
+                self.write_text(bestvals, sig_bestfit_up, sig_bestfit_down, units, fixed,runtime,errors_warnings=self.errors_warnings,fname=fname)
             
             
                 

kinms_fitter/velocity_profs.py
@@ -35,13 +35,15 @@ class velocity_profs:
             self.bincentroids=bincentroids
             self.operation="quad"
             self.labels=np.array([])
+            self.units=[]
             for i in range(0,self.freeparams):
                 self.labels=np.append(self.labels,"V"+str(i))
+                self.units.append('km/s')
 
             self.min=np.array(minimums)
             self.max=np.array(maximums)
             self.guess=np.array(guesses)
-
+            self.units=np.array(self.units)
             if np.any(fixed) == None:
                 self.fixed=np.resize(False,self.freeparams)
             else:
@@ -69,6 +71,7 @@ class velocity_profs:
             self.freeparams=1
             self.distance=distance
             self.labels=['logCentralMass']
+            self.units=['log_Msun']
             self.operation="quad"
             self.min=np.array(minimums)
             self.max=np.array(maximums)
@@ -102,10 +105,12 @@ class velocity_profs:
             if self.guess.size == 1:
                 self.freeparams=1
                 self.labels=['M/L']
+                self.units=['Msun/Lsun']
             else:
                 if self.guess.size == 2:
                     self.freeparams=2
                     self.labels=['M/L','logMBH']
+                    self.units=['Msun/Lsun','log_Msun']
                 else:
                     raise('Wrong number of guesses, expected one or two [M/L, (and optionally logBHmass)]')    
                 
@@ -149,6 +154,7 @@ class velocity_profs:
             self.operation="quad"
             self.freeparams=3
             self.labels=['M/Linner','M/Louter','logMBH']
+            self.units=['Msun/Lsun','Msun/Lsun','log_Msun']
             self.distance=distance
             self.surf=surf
             self.sigma=sigma
@@ -191,7 +197,7 @@ class velocity_profs:
             self.freeparams=2
             self.operation="quad"
             self.labels=['Vmax','Rturn']
-
+            self.units=['km/s','arcsec']
             self.min=np.array(minimums)
             self.max=np.array(maximums)
             self.guess=np.array(guesses)
@@ -222,7 +228,7 @@ class velocity_profs:
             self.freeparams=4
             self.operation="quad"
             self.labels=['Vt','Vr','Rbar','phibar']
-
+            self.units=['km/s','km/s','arcsec','deg']
             self.min=np.array(minimums)
             self.max=np.array(maximums)
             self.guess=np.array(guesses)
@@ -257,6 +263,7 @@ class velocity_profs:
             self.guess=np.array(guesses)
             self.freeparams=3
             self.labels=['mtot','re','n']
+            self.units=['log_Msun','arcsec','unitless']
             self.operation="quad"
             self.distance=distance
             self.min=np.array(minimums)
@@ -328,6 +335,7 @@ class velocity_profs:
             self.guesses=np.array(guesses)
             self.freeparams=4
             self.labels=['mtot','re_d','re_b','b_to_t']
+            self.units=['log_Msun','arcsec','arcsec','unitless']
             self.distance=distance
             self.minimums=np.array(minimums)
             self.maximums=np.array(maximums)
@@ -349,7 +357,7 @@ class velocity_profs:
                 self.precisions=np.resize(((self.max-self.min)/10.),self.freeparams)
             else:
                 self.precisions=precisions
-            #breakpoint()
+            
             self.mymodel = [velocity_profs.sersic(self.distance,guesses=[self.guesses[0]+np.log10(1-self.guesses[3]),self.guesses[1],1],minimums=[self.minimums[0]+np.log10(1-self.maximums[3]),self.minimums[1],1],maximums=[self.maximums[0]+np.log10(1-self.minimums[3]),self.maximums[1],1],fixed=[self.fixed[0]&self.fixed[3],self.fixed[1],True]),\
                        velocity_profs.sersic(self.distance,guesses=[self.guesses[0]+np.log10(self.guesses[3]),self.guesses[2],4],minimums=[self.minimums[0]+np.log10(self.maximums[3]),self.minimums[2],4],maximums=[self.maximums[0]+np.log10(self.minimums[3]),self.maximums[2],4],fixed=[self.fixed[0]&self.fixed[3],self.fixed[2],True])]
         
@@ -371,6 +379,7 @@ class velocity_profs:
             self.hubbleparam=hubbleconst
             self.operation="quad"
             self.labels=['M200','c']
+            self.units=['log_Msun','unitless']
             self.distance=distance
             self.minimums=np.array(minimums)
             self.maximums=np.array(maximums)
@@ -413,6 +422,7 @@ class velocity_profs:
             self.freeparams=3
 
             self.labels=['MLgrad','MLinter','MLendpoint']
+            self.units=['Msun/Lsun/arcsec','Msun/Lsun','arcsec']
             self.operation="mult"
             self.min=np.array(minimums)
             self.max=np.array(maximums)